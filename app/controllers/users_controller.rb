21439363bfa1c8ef0597de511e1286d1c5c35418
jrferrera <jrferrera17@gmail.com>
2014-05-02 12:35:40 +0800

    Fixed layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
 
     if user.authenticate(params[:old_password])
       user.update(password: params[:password])
+      redirect_to home_path
     end
   end
 
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to home_path, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
